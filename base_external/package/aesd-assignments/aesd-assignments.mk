d8b8364bd97b131911a67132d4e9e3f2d1fd4ca7
Michael Cook <michaeltcook98@gmail.com>
2025-06-16 19:34:04 -0700

    fixed socket init

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4cad24f5fa4e37355839fcb6904b2b6a82ffa8b3
+AESD_ASSIGNMENTS_VERSION = 821043b1f27e27e9ae095cb06496b491c1cdcb60
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
